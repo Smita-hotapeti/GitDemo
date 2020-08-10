39994dbc54ad898ef056806b708aa881b4887187
Smita-hotapeti <smit.jnc@gmail.com>
2020-08-10 22:41:45 +1000

    Commited by Smita

--- src/test/java/qaclickacademy/Mavenjava/RESTAPTITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPTITest.java
@@ -7,6 +7,8 @@ public class RESTAPTITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1.........");
+		System.out.println("postJira2............");
 	}
 	@Test
 	public void deleteTwitter() {
